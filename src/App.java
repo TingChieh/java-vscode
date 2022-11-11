8c22e06c4644b62e929d4e973e93ef9653b8a888
tingchieh <tingchieh77@outlook.com>
2022-11-11 12:27:56 +0800

    注释

--- src/App.java
+++ src/App.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-            int wsc = input.nextInt();
-            int wcg = input.nextInt();
-            int abc = wsc*wcg;
-            if (abc==888) {
-                System.out.println("该四边形为发财矩形");
-            } else {
-                System.out.println("普通矩形");
-            }
+        // Scanner input = new Scanner(System.in);
+        //     int wsc = input.nextInt();
+        //     int wcg = input.nextInt();
+        //     int abc = wsc*wcg;
+        //     if (abc==888) {
+        //         System.out.println("该四边形为发财矩形");
+        //     } else {
+        //         System.out.println("普通矩形");
+        //     }
 
 }
