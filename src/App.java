0e30f07b895e19ae36b01bdc0c5496dfccbacc4f
tingchieh <tingchieh77@outlook.com>
2022-11-14 20:39:57 +0800

    11.14练习二另一种方法

--- src/App.java
+++ src/App.java
@@ -1,21 +1,17 @@
 import java.util.Scanner;
-
-import org.junit.platform.reporting.shadow.org.opentest4j.reporting.events.java.FileEncoding;
-
-import javafx.scene.input.ScrollEvent;
-
 public class App {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        double sum = 0;
-        System.out.println("输入班级人数");
-        int num = input.nextInt();
-        for (int i = 1; i <= num; i++) {
-            System.out.println("请输入第" + i + "的成绩：");
-            double score = input.nextDouble();
-            sum = sum + score;
-            double avg = sum/num;
-            System.out.println("平均分为"+avg);
+        try (Scanner input = new Scanner(System.in)) {
+            double sum = 0;
+            System.out.println("输入班级人数");
+            int num = input.nextInt();
+            for (int i = 1; i <= num; i++) {
+                System.out.println("请输入第" + i + "的成绩：");
+                double score = input.nextDouble();
+                sum = sum + score;
+                double avg = sum/num;
+                System.out.println("平均分为"+avg);
+            }
         }
     }
 }
--- src/classpratice.java
+++ src/classpratice.java
@@ -2,12 +2,11 @@ public class classpratice {
     public static void main(String[] args) {
         double sum = 0;
         //1-20的累加
-        for(double a = 1;a <= 20;a++) {
-            if (a%5==0 || a%3==0) {
-            }else{
-                sum = sum + a;
-            }
+        for(double a = 0;a <= 20;a++) {
+            if (a%5!=0 & a%3!=0) sum += a;
+            if (sum > 100)break;
+            System.out.println(sum);
         }
-        System.out.println(sum);
+        System.out.println("\n"+sum);
     }
 }
