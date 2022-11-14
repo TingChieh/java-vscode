7fc2c26d5867c94515e48252e2f4bf37e001363e
tingchieh <tingchieh77@outlook.com>
2022-11-14 19:56:46 +0800

    11.14课堂练习2 06年6w，11年20w人

---
+++ src/classpratice.java
@@ -0,0 +1,10 @@
+public class classpratice {
+    public static void main(String[] args) {
+        int year = 2006;
+        double a = 8;
+        for (a = 8;a <= 20; year++) {
+            a = a * (1 + 0.25);
+        }
+        System.out.println("到"+year+"年人数达到20w人");
+    }
+}
