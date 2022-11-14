2d537c7878a0f95aad2e3fae6b034c7fadec52ee
tingchieh <tingchieh77@outlook.com>
2022-11-14 19:44:24 +0800

    今日习题1

--- src/App.java
+++ src/App.java
@@ -1,24 +1,21 @@
 import java.util.Scanner;
 
+import org.junit.platform.reporting.shadow.org.opentest4j.reporting.events.java.FileEncoding;
+
+import javafx.scene.input.ScrollEvent;
+
 public class App {
     public static void main(String[] args) {
-        try (Scanner input = new Scanner(System.in)) {
-            // 素数指大于1的整数中，只能被1和自身整除的正整数
-            // 输入数字a，输出1-a中的所有素数
-            int a = input.nextInt();
-            for(int b = 1;b<=a;b++) {
-                boolean f = true;
-                //定义变量f为真，假设默认b是一个素数
-            // 判断b是否是一个素数，让b分别对2到b-1取余
-            // 定义变量c来表示2到b-1这些数
-                for (int c = 2; c < b; c++) {
-                    // 如果存在c能够使得b%c==0成立
-                    // 说明b有另外一个因子c，那么b就不是一个素数，则修改f的值为假
-                    if(b % c == 0)f = false;
-                }
-                // 最后如果f为真，那么b就是一个素数
-                if(f)System.out.println(b);
-            }
+        Scanner input = new Scanner(System.in);
+        double sum = 0;
+        System.out.println("输入班级人数");
+        int num = input.nextInt();
+        for (int i = 1; i <= num; i++) {
+            System.out.println("请输入第" + i + "的成绩：");
+            double score = input.nextDouble();
+            sum = sum + score;
+            double avg = sum/num;
+            System.out.println("平均分为"+avg);
         }
     }
 }
