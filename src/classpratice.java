51ac7341585500afc765cd2179cbc0b18e8a7e3f
tingchieh <tingchieh77@outlook.com>
2022-11-15 16:03:03 +0800

    11.15 0-250度转换为华摄氏度

--- src/classpratice.java
+++ src/classpratice.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;
 public class classpratice {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String name;
-        do {
-            System.out.println("输入你的名字");
-            name = input.next();
-        } while (!name .equals("q"));
+        System.out.println("摄氏度为 华摄氏度");
+        for (double tem = 0; tem <= 250; tem = tem+20) {
+            double tempor = 0;
+            tempor = tem*9/5 + 32;
+            System.out.println(tem + "\t" + tempor);
+        }
+        System.out.println("程序结束！");
     }
 }
