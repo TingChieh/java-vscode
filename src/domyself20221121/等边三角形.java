7ddeb5e2bfbe5c657dbe3322c57fc0511f496931
tingchieh <tingchieh77@outlook.com>
2022-11-21 14:00:52 +0800

    for 循环写图形

--- src/domyself20221121/等边三角形.java
+++ src/domyself20221121/等边三角形.java
@@ -1,12 +1,12 @@
 package domyself20221121;
 
-public class test201121输出图形 {
+public class 等边三角形 {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5-i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= 5; j++) {
+            for (int j = 1; j <= 2*i-1; j++) {
                 System.out.print("*");
             }
             System.out.println();
