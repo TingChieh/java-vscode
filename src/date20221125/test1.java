7d8a522d71fa70f74c46712d4fe5cb1722b2fb13
tingchieh <tingchieh77@outlook.com>
2022-11-25 22:01:43 +0800

    20221125上课

---
+++ src/date20221125/test1.java
@@ -0,0 +1,10 @@
+package date20221125;
+
+import java.util.*;
+
+public class test1 {
+    public static void main(String[] args) {
+        int []A={1,5,9,6,7};
+        System.out.println(A[1]);
+    }
+}
