95d10f10860ca4f59f47a5e7edcba85ce9e73a04
tingchieh <tingchieh77@outlook.com>
2022-11-28 08:36:54 +0800

    20221127校园OJ

---
+++ src/domyself20221125/求最大小值.java
@@ -0,0 +1,10 @@
+package domyself20221125;
+
+import java.util.Scanner;
+
+public class 求最大小值 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("hello world6");
+    }
+}
