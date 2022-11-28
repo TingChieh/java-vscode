30c99ec58ed95eb51d4403bb239d9ca23aaa8f60
tingchieh <tingchieh77@outlook.com>
2022-11-28 09:21:32 +0800

    20221128校园OJ

---
+++ src/schoolOJ/oj1034.java
@@ -0,0 +1,13 @@
+package schoolOJ;
+
+import java.util.Scanner;
+
+public class oj1034 {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        long n = input.nextLong();
+        for (int i = 1; i <= n; i++) {
+            long j = input.nextLong();
+        }
+    }
+}
