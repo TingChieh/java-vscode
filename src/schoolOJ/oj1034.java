a351276377cd85129337e7654e184be0f506444f
tingchieh <tingchieh77@outlook.com>
2022-11-28 13:22:54 +0800

    20221128校园OJ

--- src/schoolOJ/oj1034.java
+++ src/schoolOJ/oj1034.java
@@ -1,13 +1,25 @@
 package schoolOJ;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class oj1034 {
-    public static void main(String args[]) {
-        Scanner input = new Scanner(System.in);
-        long n = input.nextLong();
-        for (int i = 1; i <= n; i++) {
-            long j = input.nextLong();
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            int a = input.nextInt();
+            int[] b = new int[a];
+            for (int i = 0; i < a; i++) {
+                b[i] = input.nextInt();
+            }
+            int min = b[0];
+            int max = b[0];
+            for (int i = 0; i < a; i++) {
+                if (min > b[i])
+                    min = b[i];
+                if (max < b[i])
+                    max = b[i];
+            }
+            System.out.println(max);
+            System.out.println(min);
         }
     }
 }
