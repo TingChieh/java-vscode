f3bafd6898b88c8663d9f380eeb42acb08ba2d5b
tingchieh <tingchieh77@outlook.com>
2022-11-29 14:06:13 +0800

    20221129冒泡排序

--- src/domyself20221128/数组中插入元素.java
+++ src/domyself20221128/数组中插入元素.java
@@ -12,27 +12,28 @@ public class 数组中插入元素 {
         list[3] = 63;
         list[4] = 60;
 
-        // 获取用户要插入的数值
-        Scanner input = new Scanner(System.in);
-        System.out.println("请输入你要插入的数值");
-        int num = input.nextInt();
-        // 找到 num 要插入的位置 index ：找到 num 比数组中元素（第一个）大的位置
-        // 当用户插入一个比原数组所有数都小的数值时，如下代码就有问题
-        // int index = 0;
-        int index= list.length - 1;
-        for (int i = 0; i < list.length; i++) {
-            if (num > list[i]) {
-                index = i;
-                break;
+        try (// 获取用户要插入的数值
+        Scanner input = new Scanner(System.in)) {
+            System.out.println("请输入你要插入的数值");
+            int num = input.nextInt();
+            // 找到 num 要插入的位置 index ：找到 num 比数组中元素（第一个）大的位置
+            // 当用户插入一个比原数组所有数都小的数值时，如下代码就有问题
+            // int index = 0;
+            int index= list.length - 1;
+            for (int i = 0; i < list.length; i++) {
+                if (num > list[i]) {
+                    index = i;
+                    break;
+                }
             }
+            // 原 index 位置及以后的所有数据要整体往后挪
+            for (int i = list.length - 1; i > index; i--) {
+                list[i] = list[i - 1];  //当list.length - 1:5  list[5] = list[4] 以此类推
+            }
+            // 在 index 位置将 num 插入进来
+            list[index] = num;
+            System.out.println("插入成绩的小标是："+index);
         }
-        // 原 index 位置及以后的所有数据要整体往后挪
-        for (int i = list.length - 1; i > index; i--) {
-            list[i] = list[i - 1];  //当list.length - 1:5  list[5] = list[4] 以此类推
-        }
-        // 在 index 位置将 num 插入进来
-        list[index] = num;
-        System.out.println("插入成绩的小标是："+index);
         System.out.println("插入数值后的最后成绩是：");
         for (int listNum : list) {
             System.out.print(listNum+"\t");
