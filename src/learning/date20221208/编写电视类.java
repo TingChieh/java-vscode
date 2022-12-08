fcdd43ec8aa41f696a6af518d7ab7d8ffba4147a
tingchieh <tingchieh77@outlook.com>
2022-12-08 20:02:22 +0800

    20221208

---
+++ src/learning/date20221208/编写电视类.java
@@ -0,0 +1,19 @@
+package learning.date20221208;
+
+public class 编写电视类 {
+    String brand;
+    int size;
+    String color;
+    public void print() {
+        System.out.println(brand);
+        System.out.println(size);
+        System.out.println(color);
+    }
+
+    public void channel() {
+        System.out.println("电视能调频道");
+    }
+    public static void main(String[] args) {
+		System.out.println("创建类成功");
+	}
+}
